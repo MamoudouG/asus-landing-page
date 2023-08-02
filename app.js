9ceca576630cd7c25027dc604177e8914cbd9536
mamoudoug <107388208+MamoudouG@users.noreply.github.com>
2023-08-02 13:37:53 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+// Menu
 console.log ('this file is working');
-
 hamburger = document.querySelector('.hamburger');
 hamburger.onclick = function() {
     navBar = document.querySelector('.nav_bar');
     navBar.classList.toggle('active');
 }
+
+// Highlight section
+
+let sections = document.querySelectorAll('section');
+ console.log(sections);
+
+ let navLikns = document.querySelectorAll('header nav li a');
+ console.log(navLikns);
+
+ window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLikns.forEach(links => {
+                links.classList.remove('active');
+                document.querySelectorAll('header nav li a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+ };
